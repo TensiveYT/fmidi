9a75db95228ab7bfd07d090f2d074371b14bb2b2
TensiveYT <TensiveYT@proton.me>
2026-06-29 18:52:13 -0400

    need to add the missing exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,50 @@
 /* import: local classes */
+import { FAudioEffect } from './classes/audio/effect/FAudioEffect.js'
+import { FAudioLimiterEffect } from './classes/audio/effect/FAudioLimiterEffect.js'
+
 import { FParser } from './classes/parser/FParser.js'
 
 import { FRenderer } from './classes/renderer/FRenderer.js'
 import { FRendererStream } from './classes/renderer/FRendererStream.js'
 
 /* import: local interfaces */
+import { FAudioLimiterOptions } from './interfaces/audio/effect/FAudioLimiterOptions.js'
+
 import { FParserTempoEvent } from './interfaces/parser/FParserTempoEvent.js'
 import { FParserThreadResponse } from './interfaces/parser/FParserThreadResponse.js'
 import { FParserTrack } from './interfaces/parser/FParserTrack.js'
 
+import { FRendererOptions } from './interfaces/renderer/FRendererOptions.js'
 import { FRendererStreamNoteEvent } from './interfaces/renderer/FRendererStreamNoteEvent.js'
+import { FRendererThread } from './interfaces/renderer/FRendererThread.js'
+
+import { FRendererSoundfontKey } from './interfaces/soundfont/FRendererSoundfontKey.js'
 
 /* import: local types */
 import { FRendererStreamEvent } from './types/renderer/FRendererStreamEvent.js'
 
 export {
 	/* export: local classes */
+	FAudioEffect,
+	FAudioLimiterEffect,
+
 	FParser,
 
 	FRenderer,
 	FRendererStream,
 
 	/* export: local interfaces */
+	FAudioLimiterOptions,
+
 	FParserTempoEvent,
 	FParserThreadResponse,
 	FParserTrack,
 
+	FRendererOptions,
 	FRendererStreamNoteEvent,
+	FRendererThread,
+
+	FRendererSoundfontKey,
 
 	/* export: local types */
 	FRendererStreamEvent
